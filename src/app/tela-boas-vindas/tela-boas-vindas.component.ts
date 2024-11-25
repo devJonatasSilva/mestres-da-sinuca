955edbc8bcdbc46d5a1a6740281efee699e406d2
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2024-11-25 18:23:00 -0300

    ajusta botoes da tela de boas vindas

--- src/app/tela-boas-vindas/tela-boas-vindas.component.ts
+++ src/app/tela-boas-vindas/tela-boas-vindas.component.ts
@@ -1,9 +1,12 @@
+import { BotaoComponent } from './../botao/botao.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tela-boas-vindas',
   standalone: true,
-  imports: [],
+  imports: [
+    BotaoComponent
+  ],
   templateUrl: './tela-boas-vindas.component.html',
   styleUrl: './tela-boas-vindas.component.css'
 })
