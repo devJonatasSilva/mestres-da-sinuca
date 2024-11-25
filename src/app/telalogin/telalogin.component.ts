a444175bbb124e023e421308af8efab9dc4e9ff4
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2024-11-25 18:23:18 -0300

    ajusta botoes da tela de login

--- src/app/telalogin/telalogin.component.ts
+++ src/app/telalogin/telalogin.component.ts
@@ -1,9 +1,10 @@
+import { BotaoComponent } from './../botao/botao.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-telalogin',
   standalone: true,
-  imports: [],
+  imports: [BotaoComponent],
   templateUrl: './telalogin.component.html',
   styleUrl: './telalogin.component.css'
 })
