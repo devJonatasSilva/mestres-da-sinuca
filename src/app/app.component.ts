e20394f5923c89d335171013e3f88d9941522551
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2024-11-27 23:07:12 -0300

    oculta a navbar nas telas de login, cadastro e boas vindas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'mestres-da-sinuca';
+
+  isOcultaNavbarNasRotas = false;
+  private rotasSemNavbar  = ['/', '/login', '/form-cadastro'];
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.isOcultaNavbarNasRotas = this.rotasSemNavbar.includes(this.router.url);
+    });
+  }
 }
