5d363006a4f04708c069126f2d8e0a2219e7f8ad
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2024-11-27 23:05:43 -0300

    inclue a rota do formulario de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import type { Routes } from '@angular/router';
 import { TelaloginComponent } from './telalogin/telalogin.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { TelaBoasVindasComponent } from './tela-boas-vindas/tela-boas-vindas.component';
+import { FormCadastroComponent } from './form-cadastro/form-cadastro.component';
 
 
 export const routes: Routes = [
   { path: '', component: TelaBoasVindasComponent },
   { path: 'login', component: TelaloginComponent },
   { path: 'home', component: HomePageComponent },
+  { path: 'form-cadastro', component: FormCadastroComponent},
 ];
