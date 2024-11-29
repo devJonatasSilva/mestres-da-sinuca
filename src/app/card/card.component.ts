a14d297e81d8b531141d682f13f695e31b3056bf
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2024-11-29 18:04:15 -0300

    ajusta os cards da home page

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrls: ['./card.component.css']
 })
 export class CardComponent {
 
+  // Definir as propriedades do jogador
+  @Input() player!: Player; // Receber as informações do jogador
+  @Input() isFirst = false; // Definir se o jogador é o 1º lugar
+  @Input() isSecond = false; // Definir se o jogador é o 2º lugar
+  @Input() isThird = false; // Definir se o jogador é o 3º lugar
+  @Input() isMVP = false; // Definir se o jogador é MVP
+
 }
+
+interface Player {
+  name: string;
+  avatarUrl: string;
+  victories: number;
+  losses: number;
+}
+
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+
+  players = [
+    {
+      name: 'Jogador 1',
+      avatarUrl: 'assets/avatar1.jpg',
+      victories: 10,
+      losses: 2,
+      isMVP: true
+    },
+    {
+      name: 'Jogador 2',
+      avatarUrl: 'assets/avatar2.jpg',
+      victories: 8,
+      losses: 4,
+      isMVP: false
+    },
+    {
+      name: 'Jogador 3',
+      avatarUrl: 'assets/avatar3.jpg',
+      victories: 7,
+      losses: 5,
+      isMVP: false
+    }
+  ];
 
 }
