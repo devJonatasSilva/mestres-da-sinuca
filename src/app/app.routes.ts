1b833a319624d6cd464cda8b37f6fdb91c1e8830
Jonatas Silva <61160773+devJonatasSilva@users.noreply.github.com>
2024-11-29 16:27:39 -0300

    inclui a rota do ranking mensal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TelaloginComponent } from './telalogin/telalogin.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { TelaBoasVindasComponent } from './tela-boas-vindas/tela-boas-vindas.component';
 import { FormCadastroComponent } from './form-cadastro/form-cadastro.component';
+import { RankingMensalComponent } from './ranking-mensal/ranking-mensal.component';
 
 
 export const routes: Routes = [
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'login', component: TelaloginComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'form-cadastro', component: FormCadastroComponent},
+  { path: 'ranking', component: RankingMensalComponent },
 ];
